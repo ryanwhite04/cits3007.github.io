212e052b5a170a3a08a1abe643e54010a8756121
arran <arranstewart@users.noreply.github.com>
2022-08-01 14:55:30 +0800

    update artifact-fetcher script

--- fetch_artifacts.sh
+++ fetch_artifacts.sh
@@ -7,12 +7,13 @@ tmpdir=$(mktemp --directory --quiet)
 
 git clone --single-branch --branch built git@github.com:cits3007/cits3007-materials.git $tmpdir
 
-mkdir -p assets/lectures assets/workshops assets/assignments
+mkdir -p assets/{lectures,workshops,labs,assignments}
 
 cp -a $tmpdir/lectures/*pdf assets/lectures/
 cp -a $tmpdir/lectures-markdown/*md assets/lectures/
-cp -a $tmpdir/workshops/*.{pdf,md,zip} assets/workshops/
-cp -a $tmpdir/assignments/*.{pdf,md} assets/assignments/
+#cp -a $tmpdir/workshops/*.{pdf,md,zip} assets/workshops/
+cp -a $tmpdir/labs/*.{html,md,zip} assets/labs/ || true
+cp -a $tmpdir/assignments/*.{pdf,md} assets/assignments/ || true
 
 rm -rf $tmpdir
 
